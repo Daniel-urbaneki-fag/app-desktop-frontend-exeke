b7f9b494ac16a90187f50c1c77a8aa0429d917e3
Daniel <dusantos@minha.fag.edu.br>
2022-09-26 19:24:09 -0300

    V1

--- main.py
+++ main.py
@@ -1,29 +1,55 @@
-# from kivy.config import Config
-# Config.set('graphics', 'width', '720')
-# Config.set('graphics', 'height', '650')
-from cgi import test
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.popup import Popup
 from kivy.uix.scrollview import ScrollView
 from kivy.core.window import Window
+import requests
+import json
 
 Builder.load_file('screens/cadastroEmpresa.kv')
 
-class Login(ScrollView):
-    def __init__(self, **kwargs): 
-        super(Login, self).__init__(**kwargs)
-        Window.softinput_mode = "below_target"
-    
-    def teste(self):
-        print(self.ids['usuario'].text)
+class CadastroEmpresa(BoxLayout):    
+    def cadastrarDados(self):
+        api = "http://127.0.0.1:5000/cadastrarEmpresa"
 
-class CadastroEmpresa(BoxLayout):
-    pass
+        if self.ids["cnpj"].text == "":
+            box = BoxLayout(orientation="vertical")
+            msg = Label(text="Cnpj vazio!")
+            box.add_widget(msg)
+            pop = Popup(title="", content=box, size_hint=(None, None), separator_height=0, background="",
+            size=(300, 60), pos_hint={"top": 0.97}, background_color=(220/255, 53/255, 69/255, 1))
+            pop.open()
+            return
+        dados_empresa = {
+            'cnpj': self.ids['cnpj'].text,
+            'razaoSocial': self.ids['razaoSocial'].text,
+            'fantasia': self.ids['fantasia'].text,
+            'telefone': self.ids['telefone'].text,
+            'e-mail': self.ids['e-mail'].text,
+            'cep': self.ids['cep'].text,
+            'logradouro': self.ids['logradouro'].text,
+            'bairro': self.ids['bairro'].text,
+            'numero': self.ids['numero'].text,
+            'cidade': self.ids['cidade'].text,
+            'tipo': self.ids['tipo'].text,
+            'matriz': self.ids['matriz'].text
+        }
+        response = requests.post(url=api, data=dados_empresa)
+        response = json.loads(response.content.decode())
+        box = BoxLayout(orientation="vertical")
+        msg = Label(text=response["msg"])
+        box.add_widget(msg)
+        pop = Popup(title="", content=box, size_hint=(None, None), separator_height=0, background="",
+            size=(300, 60), pos_hint={"top": 0.97}, background_color=response["color_msg"])
+        pop.open()
+    
     
 class ExekeApp(App):
     def build(self):
-        # Window.size = (720, 1440)
+        Window.size = (1366, 768)
+        self.icon = ('screens/icone.png')
         return CadastroEmpresa()
     
 if __name__ == '__main__':
